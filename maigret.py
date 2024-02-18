80e4359f5c755a7fe048b431367795ae189161f9
fusioncenterresources <160419863+fusioncenterresources@users.noreply.github.com>
2024-02-18 13:12:50 -0500

    Update maigret.py

--- maigret.py
+++ maigret.py
@@ -2,20 +2,24 @@
 import asyncio
 import sys
 
-from maigret.maigret import main
+from maigret.maigret import main as maigret_main
 
+async def async_run(username):
+    # Modify this line according to how main() accepts arguments
+    await maigret_main(username)
 
-def run():
+def run_maigret(username):
     try:
         if sys.version_info.minor >= 10:
-            asyncio.run(main())
+            asyncio.run(async_run(username))
         else:
             loop = asyncio.get_event_loop()
-            loop.run_until_complete(main())
+            loop.run_until_complete(async_run(username))
     except KeyboardInterrupt:
         print('Maigret is interrupted.')
         sys.exit(1)
 
-
 if __name__ == "__main__":
-    run()
+    # Assuming the first CLI argument is the username
+    username = sys.argv[1] if len(sys.argv) > 1 else ""
+    run_maigret(username)
